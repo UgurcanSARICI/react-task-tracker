6c75a584613f35274b5ea688a030a73045e8d051
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-19 16:50:57 +0300

    delete task

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,54 @@
 import React, { useState } from "react";
-import { Header } from ".";
+import { Header, AddTask, Task } from ".";
 
 const Layout = () => {
   const [isActive, setIsActive] = useState(false);
+  const [data, setData] = useState([
+    {
+      id: new Date().getMilliseconds(),
+      title: "The Alpha Team",
+      date: "Dec 12 12.00PM",
+      complated: false,
+    },
+  ]);
+
+  // Func
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("object");
+    const newTodo = {
+      id: new Date().getMilliseconds(),
+      title: e.target.taskName.value,
+      date: e.target.selectt.value,
+      complated: false,
+    };
+    setData([...data, newTodo]);
+  };
+  // Delete Func
+  const handleRemove = (id) => {
+    setData(data.filter((todo) => todo.id !== id));
+  };
+  //Func Section is Done
+
   return (
-    <div>
+    <div className="mt-[2.5rem] w-[40rem] bg-pink-500 pb-10">
       <Header active={isActive} setActive={setIsActive} />
+      {isActive && (
+        <div className="w-[30rem] mx-auto">
+          <AddTask handleSubmit={handleSubmit} />
+        </div>
+      )}
+      {data.length !== 0 ? (
+        <div className="max-h-[25rem] overflow-y-auto">
+          {data.map((item, idx) => (
+            <div key={idx}>
+              <Task data={item} handleRemove={handleRemove} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-xl font-semibold">No Task Yet</p>
+      )}
     </div>
   );
 };
